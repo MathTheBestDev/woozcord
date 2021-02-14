5b988b0151b22c88a5b5124bd7b6bc6fbc811d7d
MathTheBestDev <79018188+MathTheBestDev@users.noreply.github.com>
2021-02-14 16:22:27 +0100

    Add files via upload

--- top.php
+++ top.php
@@ -7,13 +7,12 @@
           </button>
 
           <!-- Topbar Search -->
-          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="recherche.php" method="post">
             <div class="input-group">
-              <input type="text" class="form-control bg-light border-0 small" placeholder="Rechercher un Woozen..." aria-label="Search" aria-describedby="basic-addon2">
+              <input type="text" class="form-control bg-light border-0 small" placeholder="Rechercher un Woozen..." aria-label="Search" aria-describedby="basic-addon2" name="rechercheWoozen">
               <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                  <i class="fas fa-search fa-sm"></i>
-                </button>
+                <input class="btn btn-primary" type="submit" value="RECHERCHER">
+                </input>
               </div>
             </div>
           </form>
